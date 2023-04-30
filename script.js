61a05d719aeaa8e544be92b9da35f57678f3a588
Jerico <105276338+lameons@users.noreply.github.com>
2023-04-30 14:53:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const addTask = document.querySelector('#add-task').addEventListener('click',addTASK);
+const taskContainer = document.querySelector('#task-container');
+const inputTask = document.querySelector('#input-task');
+
+function addTASK(){
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML= '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML= '<i class="fa-solid fa-trash"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if(inputTask.value ===""){
+        alert('Please Enter task')
+    }else{
+        taskContainer.appendChild(task);
+    }
+    inputTask.value=""
+
+    checkButton.addEventListener('click',
+    function(){
+        checkButton.parentElement.style.textDecoration= "line-through";
+    })
+
+    deleteButton.addEventListener('click',
+    function(e){
+        let target = e.target;
+
+        target.parentElement.parentElement.remove()
+    })
+}
